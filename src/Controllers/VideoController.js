4825b2e6a25da75f353d9cfac750bbd8d438978d
Alona Ivanova <alona20004676@gmail.com>
2024-03-14 12:26:32 +0200

    HW5_changed_resolve_with_reject

--- src/Controllers/VideoController.js
+++ src/Controllers/VideoController.js
@@ -2,7 +2,7 @@ import { uploadVideo as upload } from "../Services/VideoService.js";
 
 const uploadVideo = async (req, res) => {
     const result = await upload(req, res);
-    
+
     res.status(result.code).send(result);
 };
 
--- src/Services/VideoService.js
+++ src/Services/VideoService.js
@@ -27,17 +27,17 @@ const uploadVideo = (req, res) => {
     upload.single('video')(req, null, (err) => {
       if (err instanceof multer.MulterError) {
         if (err.code === 'LIMIT_FILE_SIZE') {
-          resolve({ code: 400, body: 'File size exceeds the limit (500Mb).' });
+          reject({ code: 400, body: 'File size exceeds the limit (500Mb).' });
         } else {
-          resolve({ code: 500, body: 'Error uploading file.' });
+          reject({ code: 500, body: 'Error uploading file.' });
         }
       } else if (err) {
-        resolve({ code: 400, body: err.message });
+        reject({ code: 400, body: err.message });
       } else {
         resolve({ code: 200, body: 'Video successfully uploaded!' });
       }
     });
-  });
+  }).catch((error) => error);
 };
 
 export { uploadVideo };
