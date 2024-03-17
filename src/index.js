8fe74d702be628a6874eebe36306d8280a9abd14
Alona Ivanova <alona20004676@gmail.com>
2024-03-17 12:56:28 +0200

    HW6_Express_investigation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 import 'dotenv/config';
 // import { app } from "./server.js";
 import { app } from "./Routes/api.js";
+import { router1, router2 } from "./Routes/routesHW6.js";
 import fs from 'fs';
+
+app.use(router1, router2);
+
+function logRequest(req, res, next) {
+  console.log(`Request made to ${req.url}`);
+  next();
+}
+
+//point 3
+//application level
+app.use(logRequest);
+//router level
+router1.use(logRequest);
+//route level
+app.get('/something', logRequest, (req, res) => {
+  res.send('something get')
+});
+
+//point 4
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong');
+});
+
+//point 5
+function validateCarId(req, res, next) {
+  const carId = req.params.carId;
+
+  if (!/^\d+$/.test(carId)) {
+      return res.status(400).json({ error: 'Car ID must be a number' });
+  }
+
+  next();
+}
+
+router2.patch('/cars/:carId', validateCarId, (req, res) => {
+  res.send('car patch')
+});
+
 
 const dirs = ['./uploads', './images'];
 
